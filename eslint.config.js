406a9cd426b1ffcb140741d88d559e157f87df0a
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-03-12 14:24:31 +0300

    update .gitignore, eslint.config, folder structure

--- eslint.config.js
+++ eslint.config.js
@@ -22,12 +22,15 @@ export default [
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "react/prop-types": 0,
     },
   },
 ]
