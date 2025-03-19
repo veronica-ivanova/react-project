fe1870a88b8aa8f994e7f85602dde726ad92139a
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-03-19 18:44:20 +0300

    upd eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from "eslint-plugin-react";
 
 export default [
   { ignores: ['dist'] },
