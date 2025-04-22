ac778839cfadad629d43d8cdc44784969a5da629
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-04-22 23:04:59 +0300

    feat: add dynamic title with generateMetadata

--- src/app/restaurants/[restaurantId]/layout.js
+++ src/app/restaurants/[restaurantId]/layout.js
@@ -1,3 +1,16 @@
 import { RestaurantLayout } from "@/components/layouts/restaurant-layout";
+import { getRestaurants } from "@/services/get-restaurants";
+
+export const generateMetadata = async ({ params }) => {
+  const { restaurantId } = await params;
+
+  const restaurants = await getRestaurants();
+
+  const restaurant = restaurants.find(({ id }) => restaurantId === id);
+
+  return {
+    title: restaurant.name,
+  };
+};
 
 export default RestaurantLayout;
